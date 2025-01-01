174280cd89cfb20ddd9cb3c9443f11bb9e80bdf1
Rahul Kumar <rahulkumar20010702@gmail.com>
2025-01-01 22:47:23 +0530

    Day 1/1/2025

---
+++ january/1/program1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Welcome" << endl
+         << "To\nThe One Technology" << "\n"
+         << "We are excited to have you on board as fullstack developer intern.\nThank You";
+    return 0;
+}
