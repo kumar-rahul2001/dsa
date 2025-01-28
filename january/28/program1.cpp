d89bf13740c91913eea15cffcc9a5476d372a230
Rahul Kumar <rahulkumar20010702@gmail.com>
2025-01-28 23:27:30 +0530

    Day 28/1/2025

---
+++ january/28/program1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float a, b, sum;
+
+    cout << "Enter two number : ";
+    cin >> a >> b;
+
+    sum = a + b;
+
+    cout << "The sum is : " << sum << endl;
+    return 0;
+}
