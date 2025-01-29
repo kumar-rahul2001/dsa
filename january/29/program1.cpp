06da1f451774fc8d40a8284f56d1bba1e7b6c1af
Rahul Kumar <rahulkumar20010702@gmail.com>
2025-01-29 23:51:54 +0530

    Day 29/01/2025

---
+++ january/29/program1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float a, b, sub;
+
+    cout << "Enter two number : ";
+    cin >> a >> b;
+
+    sub = a - b;
+
+    cout << "The substraction is : " << sub << endl;
+    return 0;
+}
